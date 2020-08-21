2211b80421b4aeca1db30df0202b1196a242bca9
Kim Bürgl <kimbuergl@gmail.com>
2020-08-21 15:11:43 +0200

    added a bit of documentation

--- src/Wikidata.jl
+++ src/Wikidata.jl
@@ -9,7 +9,13 @@ module Wikidata
     end
 
 
-
+    """
+    expects an Wikidata Identifier for a specific item as a parameter, such as WikidataEntity('Q42') for Douglas Adams
+    returns a WikidataEntity consisting of code::String and dataDict::Dict.
+    # Example
+    adams = Wikidata.WikidataEntity("Q42")
+    println(Wikidata.label(adams))
+    """
     function WikidataEntity(name::String)
         resp = HTTP.get("https://www.wikidata.org/wiki/Special:EntityData/$(name).json")
         #resp = HTTP.request("get", "www.google.com")
@@ -19,15 +25,45 @@ module Wikidata
         WikidataEntity(name, jobj)
     end
 
+    """
+    returns the english label (the title, the name) of the WikidataEntity
+    # Example
+    adams = Wikidata.WikidataEntity("Q42")
+    println(Wikidata.label(adams))
+    """
     function label(x::WikidataEntity)
-    x.dataDict["labels"]["en"]["value"]
+        x.dataDict["labels"]["en"]["value"]
 end
+    """
+    returns true, if the property for the given property identifier exists for this Entity
+    # Example
+    #load a Wikidata-Entity (Douglas Adams)
+    adams = Wikidata.WikidataEntity("Q42")
+    println(Wikidata.label(adams))
+
+    #check if place of birth exists for this entity:
+    if(Wikidata.hasproperty(adams, "P19"))
+        placeofbirth = Wikidata.getproperty(adams, "P19")
+        println(Wikidata.label(placeofbirth[1]))
+    end
+    """
     function hasproperty(x::WikidataEntity, property::String)
         return haskey(x.dataDict["claims"], property)
 end
     """
         returns a list of properties of different type.
         Supported properties: item
+
+        # Example
+        #load a Wikidata-Entity (Douglas Adams)
+        adams = Wikidata.WikidataEntity("Q42")
+        println(Wikidata.label(adams))
+
+        #check if place of birth exists for this entity:
+        if(Wikidata.hasproperty(adams, "P19"))
+            placeofbirth = Wikidata.getproperty(adams, "P19")
+            println(Wikidata.label(placeofbirth[1]))
+        end
     """
     function getproperty(x::WikidataEntity, property::String)
         if (!hasproperty(x, property))
